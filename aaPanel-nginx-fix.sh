6b33a582e1c9de32a7ce08999583dfbf7f5b394a
Broogly <157135027+broogly@users.noreply.github.com>
2024-09-18 04:39:32 +0200

    Update aaPanel-nginx-fix.sh

--- aaPanel-nginx-fix.sh
+++ aaPanel-nginx-fix.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-# Vérifier et corriger les permissions des fichiers de configuration
-echo "Vérification et correction des permissions des fichiers de configuration..."
+# Create a symbolic link for NGINX
+echo "Creating symbolic link for NGINX..."
+sudo ln -sf /www/server/nginx/sbin/nginx /usr/local/bin/nginx
+
+# Rename K01nginx_bak and S01nginx_bak files
+for dir in /etc/rc*.d; do
+  if [ -e "$dir/K01nginx_bak" ]; then
+    sudo rm -f "$dir/K01nginx"
+    sudo rename 's/K01nginx_bak/K01nginx/' "$dir/K01nginx_bak"
+  fi
+  if [ -e "$dir/S01nginx_bak" ]; then
+    sudo rm -f "$dir/S01nginx"
+    sudo rename 's/S01nginx_bak/S01nginx/' "$dir/S01nginx_bak"
+  fi
+done
+
+# Add symbolic links for run levels 2, 3, 4, and 5
+echo "Adding symbolic links for run levels 2, 3, 4, and 5..."
+sudo ln -sf ../init.d/nginx /etc/rc2.d/S01nginx
+sudo ln -sf ../init.d/nginx /etc/rc3.d/S01nginx
+sudo ln -sf ../init.d/nginx /etc/rc4.d/S01nginx
+sudo ln -sf ../init.d/nginx /etc/rc5.d/S01nginx
+
+# Configure NGINX to start automatically at system startup
+echo "Configuring NGINX to start automatically at system startup..."
+sudo update-rc.d nginx defaults
+
+# Check and correct permissions of configuration files
+echo "Checking and correcting permissions of configuration files..."
 chown root:root /etc/systemd/system/nginx.service
 chmod 644 /etc/systemd/system/nginx.service
 
-# Créer ou remplacer le fichier nginx.service avec le contenu fourni
-echo "Création ou remplacement du fichier /etc/systemd/system/nginx.service..."
+# Create or replace the nginx.service file with the provided content
+echo "Creating or replacing the /etc/systemd/system/nginx.service file..."
 cat <<EOL > /etc/systemd/system/nginx.service
 [Unit]
 Description=The NGINX HTTP and reverse proxy server
@@ -23,17 +50,16 @@ PIDFile=/www/server/nginx/logs/nginx.pid
 WantedBy=multi-user.target
 EOL
 
-# Recharger les services systemd pour prendre en compte les modifications
-echo "Rechargement des services systemd..."
+# Reload systemd services to apply changes
+echo "Reloading systemd services..."
 systemctl daemon-reload
 
-# Redémarrer le service NGINX
-echo "Redémarrage du service NGINX..."
-systemctl restart nginx
+# Enable NGINX service at startup
+echo "Enabling NGINX service..."
+systemctl enable nginx
 
-# Vérifier le statut du service NGINX
-echo "Vérification du statut du service NGINX..."
-systemctl status nginx
+# Start NGINX service
+echo "Starting NGINX service..."
+systemctl start nginx
 
-echo "Script de réparation terminé."
-
+echo "Repair script completed."
